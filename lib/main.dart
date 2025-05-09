1ccd35706d4c92c893eeaf571072c76aa86d2323
JOSELYN_PEREZ_GRANADOS <a.22308051281088@cbtis128.edu.mx>
2025-05-09 07:50:42 +0000

    cambio2

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:act6/inicial.dart';
+import 'package:act6/widget_012.dart';
+import 'package:act6/widget_013.dart';
+import 'package:act6/widget_014.dart';
+import 'package:act6/widget_015.dart';
+import 'package:act6/widget_016.dart';
+import 'package:act6/widget_017.dart';
+import 'package:act6/widget_018.dart';
+import 'package:act6/widget_019.dart';
+import 'package:act6/widget_020.dart';
+import 'package:act6/widget_021.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(Actividad());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class Actividad extends StatelessWidget {
+  const Actividad({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      debugShowCheckedModeBanner: false,
+      title: 'Widgets',
+      initialRoute: '/',
+      routes: {
+        // When navigating to the "/" route, build the FirstScreen widget.
+        '/': (context) => const Inicial(),
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+        '/pantalla2': (context) => const Widget012(),
+        '/pantalla3': (context) => const Widget013(),
+        '/pantalla4': (context) => const Widget014(),
+        '/pantalla5': (context) => const Widget015(),
+        '/pantalla6': (context) => const Widget016(),
+        '/pantalla7': (context) => const Widget017(),
+        '/pantalla8': (context) => const Widget018(),
+        '/pantalla9': (context) => const Widget019(),
+        '/pantalla10': (context) => const Widget020(),
+        '/pantalla11': (context) => const Widget021(),
+      },
     );
   }
 }
---
+++ lib/widget_021.dart
@@ -0,0 +1,47 @@
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+
+class Widget021 extends StatefulWidget {
+  const Widget021({Key? key}) : super(key: key);
+
+  @override
+  State<Widget021> createState() => _MyStatefulWidgetState();
+}
+
+/// AnimationControllers can be created with `vsync: this` because of TickerProviderStateMixin.
+class _MyStatefulWidgetState extends State<Widget021>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 10),
+    vsync: this,
+  )..repeat();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Widgeet021(controller: _controller); // ✅ Correcto
+  }
+}
+
+class Widgeet021 extends AnimatedWidget {
+  const Widgeet021({
+    Key? key,
+    required AnimationController controller,
+  }) : super(key: key, listenable: controller);
+
+  Animation<double> get _progress => listenable as Animation<double>;
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.rotate(
+      angle: _progress.value * 2.0 * math.pi,
+      child: Container(width: 200.0, height: 200.0, color: Colors.green),
+    );
+  }
+}
